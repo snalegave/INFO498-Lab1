6acaedba0a8ac4394a254d5ecec7465c14103572
snalegave <nalegave@uw.edu>
2019-04-04 12:17:51 -0700

    finished lab

---
+++ exercise/problem-a.js
@@ -0,0 +1,28 @@
+// Given an array of ints a and an int k, return if there exists a pair (x, y) in a such that x + y = k.
+// This can be solved in O(nlog(n)) time trivially, or even in O(n) if you use another data structure
+function hasPairSumToK(a, k) {
+    var s = new Set([]);
+    for (var i = 0; i < a.length; i++) {
+        if (s.has(k - a[i])) {
+            return true;
+        }
+        s.add(a[i]);
+    }
+    return false;
+}
+// TESTS don't touch them
+function test(actual, expected) {
+    if (hasPairSumToK(actual[0], actual[1]) != expected) {
+        console.log('ERROR: hasPairSumToK(', actual[0], ',', actual[1], ') should be', expected);
+    }
+    else {
+        console.log('All tests passed!');
+    }
+}
+test([[], 2], false);
+test([[-1, -2, -3], -5], true);
+test([[1, 2, 3, 4, 5, 7], 8], true);
+test([[1, 2, 3, 4, 96, -5], -4], true);
+test([[-1, -2, -3], 2], false);
+test([[1, 2, 3], 9], false);
+test([[7, 8, 9], 6], false);
